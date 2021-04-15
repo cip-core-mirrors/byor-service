50ae6a30051b2b1f23756c043186e9e7f4727110
Murat Cengiz <mcengiz2402@gmail.com>
2021-04-15 17:33:51 +0200

    Allow all hosts (CORS)

--- src/router/spreadsheet.js
+++ src/router/spreadsheet.js
@@ -8,6 +8,7 @@ router.get('/:sheetId/:range', async function(req, res, next) {
     const sheetId = req.params.sheetId;
     const range = req.params.range;
 
+    res.set('Access-Control-Allow-Origin', '*');
     const sheet = await spreadsheet.getSheet(sheetId, range);
     await res.json(sheet);
 });
