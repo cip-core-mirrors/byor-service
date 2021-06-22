b42dbc6d82a7852f01c6eededf48dbb5a90e845f
Murat Cengiz <mcengiz2402@gmail.com>
2021-06-22 15:13:45 +0200

    Debug

--- src/database/index.js
+++ src/database/index.js
@@ -86,6 +86,10 @@ async function upsert(table, columns = [], rows = []) {
 
     if (shouldLog) logQuery(sql1, rows.map(row => `(${row.join(', ')})`), sql3)
 
+    console.log('= = = = =')
+    console.log(`${format(sql1, rows)} \n${sql3}`)
+    console.log('= = = = =')
+
     if (client) return await client.query(`${format(sql1, rows)} \n${sql3}`)
 }
 
