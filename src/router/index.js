0a1fe0447ca72de80c085f86837ab504df24c57d
Murat Cengiz <mcengiz2402@gmail.com>
2021-06-23 11:19:55 +0200

    Use Google Spreadsheet endpoint only if Google-related env variables have been set

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/spreadsheet', require('./spreadsheet'));
+if (process.env.GOOGLE_CLIENT_EMAIL && process.env.GOOGLE_PRIVATE_KEY) {
+    router.use('/spreadsheet', require('./spreadsheet'));
+}
 router.use('/database', require('./database'));
 
 module.exports = router;
