5e3155748f036147f585bc52e877a9edd8a9dd80
Murat Cengiz <mcengiz2402@gmail.com>
2021-06-23 15:51:43 +0200

    SSL Root CAs package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 require('dotenv').config(); // load environment variables from .env file
+require('https').globalAgent.options.ca = require('ssl-root-cas/latest').create();
 
 async function init() {
     if (process.env.GOOGLE_CLIENT_EMAIL && process.env.GOOGLE_PRIVATE_KEY) {
