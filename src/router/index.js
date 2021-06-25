e5b3a415ccf072b4ed78e6f7eb5a1ef648de4883
Murat Cengiz <mcengiz2402@gmail.com>
2021-06-25 12:07:08 +0200

    Log all entrying calls

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use(function(req, res, next) {
+    console.log(`${new Date().toISOString()}: [${req.method}] ${req.originalUrl} ${JSON.stringify(req.body)}`);
+    next();
+});
+
 if (process.env.GOOGLE_CLIENT_EMAIL && process.env.GOOGLE_PRIVATE_KEY) {
     router.use('/spreadsheet', require('./spreadsheet'));
 }
