23c068af4ba7475644ed10d3a8508e5eb3a9f600
Murat Cengiz <mcengiz2402@gmail.com>
2021-06-25 16:50:58 +0200

    Base URL endpoint fix

--- src/router/database.js
+++ src/router/database.js
@@ -21,8 +21,6 @@ utils.init().then(async function() {
 });
 
 router.use(function(req, res, next) {
-  console.log('Database');
-
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
