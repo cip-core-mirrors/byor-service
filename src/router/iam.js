6a602dea83225076c566c5de11fbb5631f65dee9
Murat Cengiz <mcengiz2402@gmail.com>
2021-10-06 18:07:15 +0200

    Fix IAM endpoints

--- src/router/iam.js
+++ src/router/iam.js
@@ -5,86 +5,22 @@ const utils = require('../utils/iam');
 const router = express.Router();
 
 router.all('*', async function(req, res, next) {
-    return await utils.call(req.method, req.url, req.headers.authorization, req.data)
-});
-
-router.post('/roles', async function(req, res, next) {
-    const { name } = req.body;
-
-    res.set('Access-Control-Allow-Origin', '*');
     try {
-      const role = await keycloak.roles.create({
-        name: name,
-      });
-  
-      await res.json(role);
+        const response = await utils.call(req.method, req.url, req.headers.authorization, req.data);
+        return await res.json(response);
     } catch (e) {
-      const response = e.response;
-      if (response && response.data) {
-        const error = response.data.error;
-        res.status(error.code);
-        return await res.json(error.errors);
-      }
-      res.status(500);
-      await res.json(e);
-    }
-});
-
-router.put('/roles/:roleId', async function(req, res, next) {
-    const roleId = req.params.roleId;
-
-    res.set('Access-Control-Allow-Origin', '*');
-    try {
-      req.body.name = roleId;
-      const role = await keycloak.roles.updateByName(
-        { name: roleId },
-        req.body,
-      );
-  
-      await res.json(role);
-    } catch (e) {
-      const response = e.response;
-      if (response && response.data) {
-        const error = response.data.error;
-        res.status(error.code);
-        return await res.json(error.errors);
-      }
-      res.status(500);
-      await res.json(e);
-    }
-});
-
-router.put('/users/:userId/roles', async function(req, res, next) {
-    const userId = req.params.userId;
-    const role = req.body;
-
-    res.set('Access-Control-Allow-Origin', '*');
-    try {
-      const roleObject = await keycloak.roles.findOneByName({
-        name: role.name,
-      });
-      const response = await keycloak.users.addClientRoleMappings({
-        id: userId,
-        clientUniqueId: process.env.KEYCLOAK_CLIENT,
-        // at least id and name should appear
-        roles: [
-          {
-            id: roleObject.id,
-            name: roleObject.name,
-          },
-        ],
-      });
-  
-      await res.json(response);
-    } catch (e) {
-      const response = e.response;
-      if (response && response.data) {
-        const error = response.data.error;
-        res.status(error.code);
-        return await res.json(error.errors);
-      }
-      res.status(500);
-      await res.json(e);
+        console.error(e)
+        const response = e.response;
+        if (response) {
+            res.statusCode = response.status || 500;
+            if (res.headers['content-type'] === 'application/json') {
+                return await res.json(res.data);
+            }
+            await res.send(res.data);
+        } else {
+            res.statusCode = 500;
+            await res.send('Error');
+        }
     }
 });
 
