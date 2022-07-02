f4324778d53558c219419a9bc96def73b378507f
LACHANCE Patrice <patrice.lachance@socgen.com>
2022-07-02 11:10:11 +0200

    refactoring sharepoint connector: - properly handling view's ViewQuery attributes - cleaning up some vars

--- src/utils/sharepoint.js
+++ src/utils/sharepoint.js
@@ -18,68 +18,96 @@ spr = new sprequest.create({
     realm: spRealmId
 });
 
+
 async function getListItems(siteName, listName, viewName, viewFilter) {
     // Main logic
     // 1. get requested list's fields and map EntityPropertyName with Title and Title with EntityPropertyName
     // 2. get requested view's fields and build an array containing field's "Title"
     // 3. get requested list and push items in array
     console.log('[DEBUG] calling spr');
-    var url = '';
-
-    const spListUrl = spBaseUrl + '/sites/' + siteName + '/_api/web/lists/getbytitle(\'' + listName + '\')/';
-    const spFieldsFilter = '?$filter=(substringof(\'http://schemas.microsoft.com/sharepoint/v3\',SchemaXml) eq false) or (EntityPropertyName eq \'Modified\') or (EntityPropertyName eq \'Title\') or (EntityPropertyName eq \'ID\')&$select=EntityPropertyName,Title';
-    const spViewsFilter = '?$filter=Title%20eq%20\'' + viewName + '\'&$select=HtmlSchemaXml,ViewQuery';
+    const spSiteUrl = spBaseUrl + '/sites/' + siteName;
+    const spListUrl = spSiteUrl + '/_api/web/lists/getByTitle(\'' + listName + '\')';
     const spNbItems = '$top=300';
+    const spViewsFilter = '?$filter=Title%20eq%20\'' + viewName + '\'&$select=HtmlSchemaXml,ViewQuery';
     const spFilter = typeof(viewFilter) === 'undefined' ? '' : '&$filter=' + viewFilter;
+    const viewQueryUrl = spListUrl + "/Views/getbytitle('" + viewName + "')";
+    let response = '';
+  
+    // Field filter to construct EntityPropertyName <=> Title mappings
+    const spFieldsFilter = '?$filter=(substringof(\'http://schemas.microsoft.com/sharepoint/v3\',SchemaXml) eq false) or (EntityPropertyName eq \'Modified\') or (EntityPropertyName eq \'Title\') or (EntityPropertyName eq \'ID\')&$select=EntityPropertyName,Title';
 
     // Get list fields
-    url = spListUrl + 'fields' + spFieldsFilter;
-    fields = await spr.get(url);
+    url = spListUrl + '/fields' + spFieldsFilter;
+    response = await spr.get(url);
+    if (response.statusCode !== 200) {
+      console.log("ERROR: couldn't retrieve from '", url, "'!");
+    }
 
     // Generating a dictionary using the column name visible in Sharepoint List WebUI as key
     // needed to find out the corresponding 'field_x' attribute.
-    var fieldsMapping = {};
-    var namesMapping = {};
-    fields.body.d.results.map((x) => {
+    let fieldsMapping = {};
+    let namesMapping = {};
+    response.body.d.results.map((x) => {
         namesMapping[x["Title"]] = x["EntityPropertyName"];
         fieldsMapping[x["EntityPropertyName"]] = x["Title"];
     });
 
-    //console.log(fieldsMapping);
+    // Retrieve view query definition
+    response = await spr.get(viewQueryUrl);
+    if (response.statusCode !== 200) {
+        console.log("ERROR: couldn't retrieve from '", viewQueryUrl, "'!");
+    }
+    let viewDefinition = response.body.d;
 
-    // Get view fields
-    url = spListUrl + 'views' + spViewsFilter;
-    views = await spr.get(url);
-    const HtmlSchemaJson = JSON.parse(xmlToJson.xml2json(views.body.d.results[0].HtmlSchemaXml, {compact: true, spaces: 4}));
-    // Todo: handle ViewQuery parameter, using CAML
-    // check https://github.com/SharePoint/sp-dev-docs/blob/main/docs/schema/introduction-to-collaborative-application-markup-language-caml.md
-    //const viewQuery = JSON.parse(xmlToJson.xml2json(views.body.d.results[0].ViewQuery, {compact: true, spaces: 4}));
-    //console.log(HtmlSchemaJson.View.ViewFields.FieldRef);
-    //console.log(viewQuery)
+    // Extract HTML schema definition
+    const HtmlSchemaJson = JSON.parse(xmlToJson.xml2json(viewDefinition.HtmlSchemaXml, {compact: true, spaces: 4}));
 
-    var ViewFieldsMapping = {};
-    var arrViewFields = [];
+    let arrViewFieldIds = [];
+    let arrViewFieldNames = [];
     HtmlSchemaJson.View.ViewFields.FieldRef.map((x) => {
         if (x._attributes.Name === "LinkTitle") {
-            arrViewFields.push('Title');
+            arrViewFieldNames.push('Title');
         }
         else {
-            arrViewFields.push(fieldsMapping[x._attributes.Name]);
+            arrViewFieldNames.push(fieldsMapping[x._attributes.Name]);
         }
+        arrViewFieldIds.push(x._attributes.Name);
     })
-    //console.log(arrViewFields);
+    
+    const viewXml = '<View><Query>' + viewDefinition.ViewQuery + '</Query></View>';
+    const itemsUrl = spSiteUrl + "/_api/web/lists/getbytitle('" + listName + "')/getitems?$select=" + arrViewFieldIds.join(','); 
+    //var itemsUrl = spSiteUrl + "/_api/web/lists/getbytitle('" + listName + "')/getitems?$select=field_3"; 
+    const queryPayload = {  
+               'query' : {
+                      '__metadata': { 'type': 'SP.CamlQuery' }, 
+                      'ViewXml' : viewXml  
+               }
+    };
 
+    // Get a request digest
+    let spRequestDigest = await spr.requestDigest(spSiteUrl);
+    
+    // Pull items
+    response = await spr.post(itemsUrl, {
+        body: queryPayload,
+        headers: {
+          'X-RequestDigest': spRequestDigest,
+          'Accept': 'application/json; odata=verbose',
+          'content-type': 'application/json; odata=verbose'
+        }
+    });
+    if (response.statusCode !== 200) {
+        console.log("ERROR: couldn't retrieve from '", itemsUrl, "'!");
+    }
 
-    // generate list of columns from the view
-    url = spListUrl + 'items?viewid=' + HtmlSchemaJson.View._attributes.Name + spFilter + "&" + spNbItems;
-    items  = await spr.get(url);
+    let viewItems = response.body.d.results;
 
     //Use the mapping to appropriately rearrange the value array before returning
-    radarData = items.body.d.results.map(x => {
+    radarData = viewItems.map(x => {
         arr =[];
-        for (let i = 0; i < arrViewFields.length; i++) {
+        for (let i = 0; i < arrViewFieldNames.length; i++) {
             // add value to array
-            arr.push(x[namesMapping[arrViewFields[i]]])
+            arr.push(x[namesMapping[arrViewFieldNames[i]]])
             //console.log(arr);
         }
     
@@ -87,9 +115,9 @@ async function getListItems(siteName, listName, viewName, viewFilter) {
     })
     
     // Check if we got special backend directives
-    var index = 0;
+    let index = 0;
     while (radarData[index]) {
-        var arr = radarData[index];
+        let arr = radarData[index];
         
         // remove entry, as explicitely requested!
         if (arr.includes('byorBackendExcludeEntry') ) {
@@ -102,14 +130,14 @@ async function getListItems(siteName, listName, viewName, viewFilter) {
             //console.log('found!');
             for (let j = 0; j < arr.length; j++) {
                 let fieldValue = arr[j];
-                let fieldName = namesMapping[arrViewFields[j]];
+                let fieldName = namesMapping[arrViewFieldNames[j]];
                 // Modified SharePoint entries will always have a value, including our special 
                 // metadata entry, so excluding it
                 if ( (fieldValue !== 'byorBackendRenameColumn') && (fieldValue !== null) && (fieldName !== 'Modified')) {
                     let index = -1;
-                    index = arrViewFields.indexOf(arrViewFields[j]);
+                    index = arrViewFieldNames.indexOf(arrViewFieldNames[j]);
                     if (index !== -1) {
-                        arrViewFields[index] = fieldValue;
+                        arrViewFieldNames[index] = fieldValue;
                     }
                 }
             }
@@ -121,11 +149,11 @@ async function getListItems(siteName, listName, viewName, viewFilter) {
     }
     // Renaming some additional fields
     index = -1;
-    index = arrViewFields.indexOf('Modified');
+    index = arrViewFieldNames.indexOf('Modified');
     if (index !== -1) {
-        arrViewFields[index] = 'lastupdate';
+        arrViewFieldNames[index] = 'lastupdate';
     }
-    radarData.unshift(arrViewFields);
+    radarData.unshift(arrViewFieldNames);
     return radarData || [];
 }
 
